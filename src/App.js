7a950551a872b3460cc0bf84878efb23d11c23a5
Michael Mueller <michael@miniguide.co>
2020-05-08 21:20:00 +0200

    simplifying format of apiClient methods in app

--- src/App.js
+++ src/App.js
@@ -8,81 +8,42 @@ import Events from './views/Events';
 import './App.css';
 
 class App extends Component {
-  state = {
-    status: 'loading',
-    user: null,
-  };
+  state = { status: 'loading', user: null };
 
   componentDidMount() {
     apiClient
       .whoami()
-      .then((user) => {
-        this.setState({
-          status: 'loggedIn',
-          user,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          status: 'error',
-          user: null,
-        });
+      .then((user) => this.setState({ status: 'loggedIn', user }))
+      .catch(({ response }) => {
+        if (response.status === 401) this.setState({ status: 'loggedOut', user: null });
+        else this.setState({ status: 'error', user: null });
       });
-  }
+  };
 
   handleSignIn = ({ username, password }) => {
     apiClient
       .signIn({ username, password })
-      .then(({ data: user }) => {
-        this.setState({
-          status: 'loggedIn',
-          user,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          status: 'error',
-          user: null,
-        });
-      });
+      .then(({ data: user }) => this.setState({ status: 'loggedIn', user }))
+      .catch((error) => this.setState({ status: 'error', user: null }));
   };
 
   handleRegister = ({ username, password }) => {
     apiClient
       .register({ username, password })
-      .then(({ data: user }) => {
-        this.setState({
-          status: 'loggedIn',
-          user,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          status: 'error',
-          user: null,
-        });
-      });
+      .then(({ data: user }) => this.setState({ status: 'loggedIn', user }))
+      .catch((error) => this.setState({ status: 'error', user: null }));
   };
 
   logout = () => {
     apiClient
       .logout()
-      .then(() => {
-        this.setState({
-          status: 'loggedOut',
-          user: null,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          status: 'error',
-          user: null,
-        });
-      });
-  }
+      .then(() => this.setState({ status: 'loggedOut', user: null }))
+      .catch((error) => this.setState({ status: 'error', user: null }));
+  };
 
   render() {
     const { status } = this.state;
+    console.log('status', status);
     return (
       <div className='App'>
         {status === 'loading' && <div> Loading.......</div>}
