8733932a10c446c67da99841b36b0e89d6b643d6
Michael Mueller <michael@miniguide.co>
2020-05-09 11:52:22 +0200

    use indices to clean up imports

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch } from 'react-router-dom';
-import AnonRoute from './components/AnonRoute';
-import PrivateRoute from './components/PrivateRoute';
+import { AnonRoute, PrivateRoute } from './components';
 import AuthProvider from './context/authContext';
-import Home from './views/Home';
-import Register from './views/Register';
-import Events from './views/Events';
+import { Events, Home, Register } from './views';
 import './App.css';
 
-class App extends Component {
-
-  render() {
-    return (
-      <AuthProvider>
-        <div className='App'>
-          <Switch>
-            <AnonRoute exact path='/' component={Home} />
-            <AnonRoute exact path='/register' component={Register} />
-            <PrivateRoute exact path='/events' component={Events} />
-          </Switch>
-        </div>
-      </AuthProvider>
-    )
-  }
+const App = () => {
+  return (
+    <AuthProvider>
+      <div className='App'>
+        <Switch>
+          <AnonRoute exact path='/' component={Home} />
+          <AnonRoute exact path='/register' component={Register} />
+          <PrivateRoute exact path='/events' component={Events} />
+        </Switch>
+      </div>
+    </AuthProvider>
+  )
 }
 
 export default App;
---
+++ src/views/index.js
@@ -0,0 +1,3 @@
+export { default as Events } from './Events';
+export { default as Home } from './Home';
+export { default as Register } from './Register';
