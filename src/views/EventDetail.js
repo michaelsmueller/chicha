2085b21db047ed62278d5bff0989c8cd5fae01ff
Michael Mueller <michael@miniguide.co>
2020-05-13 04:09:51 +0200

    add ticket_uri

--- src/views/EventDetail.js
+++ src/views/EventDetail.js
@@ -29,7 +29,7 @@ export default class EventDetail extends Component {
 }
 
 const EventPage = ({ event }) => {
-  const { data: { name, description, cover, start_time, end_time, place } } = event;
+  const { data: { name, description, cover, start_time, end_time, ticket_uri, place } } = event;
   return (
     <div className='event-preview'>
       <div className='event-image-container'>
@@ -39,6 +39,7 @@ const EventPage = ({ event }) => {
         <h1 className='name'>{name}</h1>
         <h2 className='times'>{start_time} – {end_time}</h2>
         <h2 className='place'>{place.name}</h2>
+        {ticket_uri && <h2 className='tickets'><a href={ticket_uri}>Tickets</a></h2>}
         <p className='street'>{place.street}</p>
         <p className='description'>{description}</p>
       </div>
