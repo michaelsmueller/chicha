9f3727d462e4adc0e4b0e22c51a8fa5361b3879c
Michael Mueller <michael@miniguide.co>
2020-05-18 15:37:14 +0200

    create vote and remove vote working for both upvote and downvote

--- src/views/VoteButtons.js
+++ src/views/VoteButtons.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import apiClient from '../services/apiClient';
 
 export default class VoteButtons extends Component {
-  state = { direction: 0, total: 0 };
+  state = { vote: undefined, total: 0 };
 
   handleUpvote = async (e) => {
     console.log('upvoting');
     e.preventDefault();
     const { event: { _id: eventId } } = this.props;
-    const { direction } = this.state;
+    const { vote } = this.state;
+    const direction = vote?.direction || 0;
     try {
       switch (direction) {
         case -1:
-          // await apiClient.changeVote({ id, direction: 1 });
+          const { _id: id } = vote;
+          await apiClient.changeVote(id, { eventId, direction: 1 });
           // this.setState({ direction: 1 });
           break;
         case 0:
-          await apiClient.createVote({ eventId, direction: 1 });
-          this.setState({ direction: 1 });
+          const createVoteResponse = await apiClient.createVote({ eventId, direction: 1 });
+          this.setState({ vote: createVoteResponse.data.newVote });
           break;
         case 1:
-          const { _id: id } = this.props.vote;
-          await apiClient.removeVote(id);
-          this.setState({ direction: 0 });
+          await apiClient.removeVote(vote._id);
+          this.setState({ vote: undefined });
           break;
         default:
           throw new Error('error in vote state');
@@ -32,26 +33,40 @@ export default class VoteButtons extends Component {
     }
   }
 
-  handleDownvote = (e) => {
+  handleDownvote = async (e) => {
     console.log('downvoting');
     e.preventDefault();
-    const { event: { _id: id } } = this.props;
-    let { vote } = this.state;
-    let direction;
-    if (vote > -1) direction = -1;
-    else direction = 0;
-    apiClient.vote({ id, direction })
-      .then((response) => this.setState({ vote: direction }))
-      .catch((error) => console.log(error))
+    const { event: { _id: eventId } } = this.props;
+    const { vote } = this.state;
+    const direction = vote?.direction || 0;
+    try {
+      switch (direction) {
+        case -1:
+          await apiClient.removeVote(vote._id);
+          this.setState({ vote: undefined });
+          break;
+        case 0:
+          const createVoteResponse = await apiClient.createVote({ eventId, direction: -1 });
+          this.setState({ vote: createVoteResponse.data.newVote });
+          break;
+        case 1:
+          const { _id: id } = vote;
+          await apiClient.changeVote(id, { eventId, direction: -1 });
+          // this.setState({ direction: 1 });
+          break;
+        default:
+          throw new Error('error in vote state');
+      };
+    } catch(error) {
+      console.log(error);
+    }
   }
 
-  componentDidMount = () => {
-    const { vote } = this.props;
-    if (vote) this.setState({ direction: vote.direction });
-  }
+  componentDidMount = () => this.setState({ vote: this.props.vote });
 
   render() {
-    const { direction } = this.state;
+    const { vote } = this.state;
+    const direction = vote?.direction || 0;
     return (
       <div className='votes'>
         <div>vote direction {direction} </div>
