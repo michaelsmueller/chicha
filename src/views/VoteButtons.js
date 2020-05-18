87af6754ba4c0c8df2eeae371322c2f5bc8934f0
Michael Mueller <michael@miniguide.co>
2020-05-18 12:21:59 +0200

    read in vote state, update VoteButtons component state

--- src/views/VoteButtons.js
+++ src/views/VoteButtons.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import apiClient from '../services/apiClient';
 
 export default class VoteButtons extends Component {
-  state = { direction: 0, total: 0 };
+  state = { vote: 0, total: 0 };
 
   handleUpvote = (e) => {
     console.log('upvoting');
     e.preventDefault();
     const { event: { _id: id } } = this.props;
-    let { direction } = this.state;
-    if (direction < 1) direction = 1;
+    const { vote } = this.state;
+    let direction;
+    if (vote < 1) direction = 1;
     else direction = 0;
     apiClient.vote({ id, direction })
-      .then((response) => this.setState({ direction }))
+      .then((response) => this.setState({ vote: direction }))
       .catch((error) => console.log(error))
   }
 
@@ -20,32 +21,39 @@ export default class VoteButtons extends Component {
     console.log('downvoting');
     e.preventDefault();
     const { event: { _id: id } } = this.props;
-    let { direction } = this.state;
-    if (direction > -1) direction = -1;
+    let { vote } = this.state;
+    let direction;
+    if (vote > -1) direction = -1;
     else direction = 0;
     apiClient.vote({ id, direction })
-      .then((response) => this.setState({ direction }))
+      .then((response) => this.setState({ vote: direction }))
       .catch((error) => console.log(error))
   }
 
   componentDidMount = () => {
-    const { user, event } = this.props;
-    console.log('user', user);
-    console.log('event', event);
-    if (user.votes.length) {
-      console.log('user has votes');
+    const { user, user: { votes }, event, event: { _id: eventId } } = this.props;
+    // console.log('user', user);
+    // console.log('event', event);
+    if (votes.length) {
+      const voteOnThisEvent = votes.filter((vote) => vote.event === eventId);
+      if (voteOnThisEvent.length) {
+        const { vote } = voteOnThisEvent[0];
+        console.log('vote is', vote);
+        this.setState({ vote });
+        console.log('matching vote', voteOnThisEvent);
+      }
     } else {
-      console.log('user has no votes');
+      // console.log('user has no votes');
     }
     // this.setState({ event: this.props.event });
   }
 
   render() {
     const { upvotes, downvotes } = this.props.event;
-    const { direction } = this.state;
+    const { vote } = this.state;
     return (
       <div className='votes'>
-        <div>direction {direction} </div>
+        <div>vote state {vote} </div>
         <button onClick={this.handleUpvote}>↑</button>
         <div>{upvotes || 0 - downvotes || 0}</div>
         <button onClick={this.handleDownvote}>↓</button>
