4b66d1241d7fec180691ff82efe1095933180ab1
Michael Mueller <michael@miniguide.co>
2020-06-01 20:51:38 +0200

    style qr-code

--- src/views/offers/CouponsList.js
+++ src/views/offers/CouponsList.js
@@ -19,16 +19,15 @@ const CouponPreviews = ({ coupons }) => {
 
 const CouponPreview = ({ coupon }) => {
   const { _id: couponId, offer } = coupon;
-  const { partner, image, description, cost } = offer || '';
+  const { partner, description } = offer || '';
   return (
-    <div className='coupon-preview'>
-        {image && <img alt={partner} src={image} />}
-        <div className='coupon-info'>
-          {partner && <p className='partner'>{partner}</p>}
-          {description && <p className='description'>{description}</p>}
-        </div>
-        <QRCode value={'http://localhost:3000/events/5ebb4e71532ad00017e677f1'} renderAs='svg' />
-        <div className='cost'>{cost}</div>
+    <div className='coupon-preview-container'>
+      <div className='qr-code'><QRCode value={couponId} renderAs='svg' /></div>
+      <div className='coupon-preview'>
+        {partner && <div className='partner'>{partner}</div>}
+        {description && <div className='description'>{description}</div>}
+        <div className='coupon-id'>Your reedeem code: <br /> {couponId}</div>
+      </div>
     </div>
   );
 };
