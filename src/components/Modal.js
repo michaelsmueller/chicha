1747b995e9ca44379a97e42a9fe58f0d305ea903
Michael Mueller <michael@miniguide.co>
2020-06-03 00:55:19 +0200

    scan qr code in legacy mode so it works in chrome

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,7 +8,7 @@ const Modal = ({ activeModal, onClose, title, onClear, children }) => {
       <div className='header'>
         <button className='close' onClick={onClose}><i className='material-icons'>close</i></button>
         <h1 className='title'>{getTitle(title)}</h1>
-        {onClear && <button className='clear' onClick={onClear}>Clear</button>}
+        <div className='clear'>{onClear && <button className='clear' onClick={onClear}>Clear</button>}</div>
       </div>
       {children}
     </div>
--- src/views/offers/ScanCoupon.js
+++ src/views/offers/ScanCoupon.js
@@ -13,10 +13,13 @@ export default class ScanCoupon extends Component {
 
   handleError = (error) => console.log(error);
 
+  openImageDialog = () => this.refs.qrReader1.openImageDialog();
+
   render () {
     return (
       <div className='scan-coupon'>
-        <QrReader delay={300} onError={this.handleError} onScan={this.handleScan} style={{ width: '100%' }} />
+        <QrReader ref='qrReader1' delay={300} onError={this.handleError} onScan={this.handleScan} style={{ width: '100%' }} legacyMode />
+        <input type="button" value="Submit QR Code" onClick={this.openImageDialog} />
       </div>
     );
   };
